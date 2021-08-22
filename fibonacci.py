b774cc6e52e2900bfd0262936abdc92abad598da
Naiwrit Mullick <naiwrit.wrick@gmail.com>
2021-08-22 11:49:56 +0530

    Added a function for fibonacci operation

--- fibonacci.py
+++ fibonacci.py
@@ -11,23 +11,23 @@
 #     f1 = f2
 #     f2 = f3
 
-f1 = 0
-f2 = 1
-num = int(input("Enter the number: "))
-#print(a,b,end =' ') #end is basically for formatting used in the print fucntion to input space instead of next line
-
-count = sum = 0
-
-if num <= 0:
+def fibo_series(num):
+    f1 = 0
+    f2 = 1
+    count = sum = 0
+    if num <= 0:
     print("wrong")
-elif num == 1:
-    print("Fibonacci series: ",num)
-    print(f1)
-else:
-    print("Fibonacci series")
-    while count < num:
+    elif num == 1:
+        print("Fibonacci series: ",num)
         print(f1)
-        sum = f1 + f2
-        f1 = f2
-        f2 = sum
-        count = count + 1
+    else:
+        print("Fibonacci series")
+        while count < num:
+            print(f1)
+            sum = f1 + f2
+            f1 = f2
+            f2 = sum
+            count = count + 1
+
+num = int(input("Enter the number: "))
+fibo_series(num)
