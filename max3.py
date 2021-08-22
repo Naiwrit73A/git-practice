09678a4e286b5caa417db1a5deac14a10a9bd528
Naiwrit Mullick <naiwrit.wrick@gmail.com>
2021-08-22 12:01:00 +0530

    Update max3.py

--- max3.py
+++ max3.py
@@ -4,7 +4,7 @@ num3 = int(input("Enter 3rd number: "))
 
 if num1 > num2 and num1 > num3:
     print("The largest number is:", num1)
-elif num2 > num1 and num2 > num3:
+elif num2 > num3:
     print("The largest number is:", num2)
 else:
     print("The largest number is:", num3)
